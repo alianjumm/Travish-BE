cde46b57ccad7b0cc56209d210ad55d391913dae
Ali Anjum <iianjum@hotmail.com>
2022-04-27 03:27:39 -0400

    checkpoiint v

--- routes/vacation.js
+++ routes/vacation.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 const vacationCntrl = require("../controllers/vacation");
 
-router.post("/vacation/add", isLoggedIn, vacationCntrl.vacation_create_post);
+router.post("/vacation/add", vacationCntrl.vacation_create_post);
 router.get("/vacation/index", vacationCntrl.vacation_index_get);
 router.get("/vacation/detail", vacationCntrl.vacation_show_get);
-router.get("/vacation/delete", vacationCntrl.vacation_delete_get);
+router.delete("/vacation/delete", vacationCntrl.vacation_delete_get);
 router.get("/vacation/edit", vacationCntrl.vacation_edit_get);
 router.put("/vacation/update", vacationCntrl.vacation_update_put);
 
--- routes/wishList.js
+++ routes/wishList.js
@@ -4,11 +4,11 @@ const isLoggedIn = require("../helper/isLoggedIn");
 const wishListCntrl = require('../controllers/wishList');   
 
 
-router.get("/wishList/index", isLoggedIn, wishListCntrl.wishList_index_get);
-router.post("/wishList/add", isLoggedIn, wishListCntrl.wishList_create_post);
-router.get("/wishList/detail", isLoggedIn, wishListCntrl.wishList_show_get);
-router.delete("/wishList/delete", isLoggedIn, wishListCntrl.wishList_delete_get);
-router.get("/wishList/edit", isLoggedIn, wishListCntrl.wishList_edit_get);
-router.put("/wishList/update", isLoggedIn, wishListCntrl.wishList_update_put);
+router.get("/wishList/index",  wishListCntrl.wishList_index_get);
+router.post("/wishList/add",  wishListCntrl.wishList_create_post);
+router.get("/wishList/detail",  wishListCntrl.wishList_show_get);
+router.delete("/wishList/delete",  wishListCntrl.wishList_delete_get);
+router.get("/wishList/edit",  wishListCntrl.wishList_edit_get);
+router.put("/wishList/update",  wishListCntrl.wishList_update_put);
 
 module.exports = router;
